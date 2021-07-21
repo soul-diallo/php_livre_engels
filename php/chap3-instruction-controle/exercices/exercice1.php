3ebe1214894d4f2d9fdc8f02b896d86e802146f2
@SoulOG <285963@supinfo.com>
2021-07-21 23:59:27 +0000

    ajout de lexo 1

---
+++ php/chap3-instruction-controle/exercices/exercice1.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Redigez une expression conditionnelle pour tester si un nopmbre est a la fois un multiple de 3 et 5
+*/
+
+
--- php/chap3-instruction-controle/exercices/exercice4.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Determinez le numero de version de PHP, le nom du systeme d'exploitation de votre serveur ainsi
-que la langue du navigateur du poste client
-*/
-
-$x = "PHP7";
-$a[] = &$x;
-echo $a;
