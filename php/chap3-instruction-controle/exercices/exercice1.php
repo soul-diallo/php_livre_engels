2b60fc61a4d70a77c79c98264b88779e425c89b4
@SoulOG <285963@supinfo.com>
2021-07-22 00:09:04 +0000

    Fin de l'exo 1

--- php/chap3-instruction-controle/exercices/exercice1.php
+++ php/chap3-instruction-controle/exercices/exercice1.php
@@ -1,6 +1,11 @@
 <?php
 /*
-Redigez une expression conditionnelle pour tester si un nopmbre est a la fois un multiple de 3 et 5
+Redigez une expression conditionnelle pour tester si un nombre est a la fois un multiple de 3 et 5
 */
 
+$nombre = 14;
 
+if ($nombre%5==0 and $nombre%3==0){
+    echo "Ce nombre est a la fois multiple de 3 et 5";
+}
+else echo "Ce nombre n'est pas un multiple de 3 et 5";
