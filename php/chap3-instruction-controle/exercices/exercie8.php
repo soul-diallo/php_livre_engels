42d9db3dcea0c63b1b3fb7238b28c3927de600f1
@SoulOG <285963@supinfo.com>
2021-07-24 18:50:30 +0000

    fin du chapitre 3

---
+++ php/chap3-instruction-controle/exercices/exercie8.php
@@ -0,0 +1,12 @@
+<?php
+/* Rechercher le PGCD de deux nombres donnés. Gérez au moyen d'une exception le cas ou au moins un des nombres
+ * n'est pas entier.
+*/
+
+$nombreA = 25;
+$nombreB = 35;
+
+// Recherche du PGCD
+for ($i=0;$i<=$nombreA/2;$i++){
+    
+}
