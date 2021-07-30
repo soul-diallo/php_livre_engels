50bf72611fe1e6c7b3b9f5528262e9d7a5926bc4
@SoulOG <285963@supinfo.com>
2021-07-30 00:31:34 +0000

    Fin de l'exercice 1

--- php/chap5-les-tableaux/exercices/exercice1.php
+++ php/chap5-les-tableaux/exercices/exercice1.php
@@ -1,1 +1,14 @@
 <?php
+/* Ecrivez un tableau multidimensionnel associatif dont les cles sont des noms de personne
+ * et les valeurs des tableaux indices contenant le prenom, la ville de residence et l'age
+ * de la personne.
+ */
+
+$tableau = [
+    ["Diallo"=>["Souleymane","Conakry",23]],
+    ["Uzumaki"=>["Naruto","Konoha",25]],
+    ["Nara"=>["Shikamaru","Konoha",26]],
+    ["Kinzoko"=>["Batto","Arc City",29]],
+    ["Tradarzi"=>["Gallopacho","Napoli",35]]
+];
+
