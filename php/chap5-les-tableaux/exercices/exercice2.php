bd7e88fff27a272c9770886a9b94e91773830226
@SoulOG <285963@supinfo.com>
2021-07-30 01:00:49 +0000

    Fin de l'exercice 2

---
+++ php/chap5-les-tableaux/exercices/exercice2.php
@@ -0,0 +1,30 @@
+<?php
+
+/* Ecrivez un tableau multidimentionnel associatif dont les cles sont des noms de personne et les valeurs
+ * des tableaux associatifs dont les cles sont le prenom, la ville de residence et l'age de la personne avec
+ * une serie de valeurs associees.
+ */
+
+$tableau_multi = array(
+    "Barry" => array(
+        "prenom"=>"Aissatou",
+        "ville"=>"Toronto",
+        "age"=>22
+    ),
+    "Sabri" => array(
+        "prenom"=>"Gastalle",
+        "ville"=>"Prude",
+        "age"=>27
+    ),
+    "King" => array(
+        "prenom"=>"Vato",
+        "ville"=>"Milano",
+        "age"=>23
+    ),
+    "Porticima" => array(
+        "prenom"=>"Alajadrazo",
+        "ville"=>"Pilatania",
+        "age"=>25
+    ),
+);
+
